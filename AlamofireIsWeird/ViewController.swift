2171334c9a27d9063483f88df6d9164299d0eb14
Ronak Shah <ronakshahultimate@gmail.com>
2019-03-02 18:41:34 -0800

    Initial Commit

---
+++ AlamofireIsWeird/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofireIsWeird
+//
+//  Created by Ronak Shah on 3/2/19.
+//  Copyright © 2019 Ronak Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
